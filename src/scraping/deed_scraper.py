5e8b805505e62fab6b02940d5cd34bdbeb313a09
glb32 <41449205+glb32@users.noreply.github.com>
2021-12-25 03:56:13 +0100

    refactoring complete
    TODO: MongoDB implementation

--- src/scraping/deed_scraper.py
+++ src/scraping/deed_scraper.py
@@ -10,38 +10,6 @@ def replaceSubstrs(substring_list,replace,string):
         string = string.replace(subs,replace)
     return string
 
-#fetch a deed
-def getDeed(page,s,base):
-    #get data
-    url = "{}/index.cfm?zaction=AUCTION&Zmethod=UPDATE&FNC=LOAD&AREA=W&PageDir=".format(base) + str(page) + "&doR=0&tx=" + str(nowMilliseconds()) + "&bypassPage=0"
-    cookies = s.cookies.get_dict()
-    result = s.get(url,cookies=cookies).text
-    
-    
-    #parse data
-    raw = replaceSubstrs(["@A","@B","@C","@D","@E","@F","@G","@H","@I","@J","@K","@L"],"",result)
-    raw = raw.replace("\\\"","\"")
-    raw = raw.strip()
-    soup = BeautifulSoup(raw,features='lxml')
-    cases = [element.next_sibling.text for element in soup.find_all('th',{'aria-label':'Case Number'})]
-    bids = [element.next_sibling.text for element in soup.find_all(lambda tag:tag.name=="th" and "Opening Bid:" in tag.text)]
-    urls = [element['href'] for element in soup.find_all('a',{'onclick':'return showExitPopup();'})]
-    addresses = [str(element.next_sibling.text + ' ' + element.next.next.next.next.text) for element in soup.find_all(lambda tag:tag.name=="th" and "Property Address:" in tag.text)]
-    assessed_values = [element.next_sibling.text for element in soup.find_all(lambda tag:tag.name=="th" and "Assessed Value:" in tag.text)]
-    
-    return dict(cases,bids,urls,addresses,assessed_values)
-#change deed page
-def change(baseUrl,url,direction=1):
-    s = requests.Session()
-    s.get(url)
-    prev_auction =getDeed(0,s,baseUrl)
-
-    while True:
-        if getDeed(direction,s,baseUrl) == prev_auction:
-            break
-        else:
-            prev_auction = getDeed(direction,s,baseUrl)
-
 def getAndParseAuctionHTML(session,base,direction):
     #get data
     url = "{}/index.cfm?zaction=AUCTION&Zmethod=UPDATE&FNC=LOAD&AREA=W&PageDir=".format(base) + str(direction) + "&doR=0&tx=" + str(nowMilliseconds()) + "&bypassPage=0"
@@ -52,12 +20,24 @@ def getAndParseAuctionHTML(session,base,direction):
     raw = raw.strip()
     return raw
 
-def parseDeeds(base):
-
+def parseDeeds(baseUrl,auctionUrl):
     
+    session=requests.Session()
+    session.get(auctionUrl)
+    raw = getAndParseAuctionHTML(session=session,base=baseUrl,direction=0)
+    
+    while True:
+        next_call = getAndParseAuctionHTML(session,baseUrl,1)
+        if next_call in raw:
+            break
+        else:
+            raw = raw+"\n"+next_call
+
     soup = BeautifulSoup(raw,features='lxml')
     cases = [element.next_sibling.text for element in soup.find_all('th',{'aria-label':'Case Number'})]
     bids = [element.next_sibling.text for element in soup.find_all(lambda tag:tag.name=="th" and "Opening Bid:" in tag.text)]
     urls = [element['href'] for element in soup.find_all('a',{'onclick':'return showExitPopup();'})]
     addresses = [str(element.next_sibling.text + ' ' + element.next.next.next.next.text) for element in soup.find_all(lambda tag:tag.name=="th" and "Property Address:" in tag.text)]
     assessed_values = [element.next_sibling.text for element in soup.find_all(lambda tag:tag.name=="th" and "Assessed Value:" in tag.text)]
+    
+    return (cases,bids,urls,addresses,map(price_parser.parser.parse_price,assessed_values))
