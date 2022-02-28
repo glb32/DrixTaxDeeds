e1fbe7f7b178a570fe3b8744460c4263d99be746
glb32 <41449205+glb32@users.noreply.github.com>
2022-02-28 23:03:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ token = input("Please input your token:")
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=[618048657001938974])
+@bot.slash_command(guild_ids=[922599971461672961])
 async def fetch_deeds_by_date_and_price(ctx, time: Option(str, "Time range. Format = 2022/2/20 2022/2/22",Required=True),price: Option(str,"Price Range. format: 250 30000",Required=False)):
     data = db.fetchDeedsInDesiredRange(calendar.timegm(parser.parse(time.split(" ")[0]).timetuple()) , calendar.timegm(parser.parse(time.split(" ")[1]).timetuple()), [int(price.split(" ")[0]),int(price.split(" ")[1])])
     for auction in range(len(data)):
@@ -21,10 +21,10 @@ async def fetch_deeds_by_date_and_price(ctx, time: Option(str, "Time range. Form
             embeds.add_field(name="Parcel URL:", value=deed['url'],inline=True)
             embeds.add_field(name="Property Address:", value=deed['property_address'],inline=True)
             embeds.add_field(name="Assessed Value:", value=deed['assessed_value'],inline=True)
-            embeds.add_field(name="Associated Website (County/Location)",value=data[auction]['location'])
+            #embeds.add_field(name="Associated Website (County/Location)",value=data[auction]['location'])
             await ctx.send(embed=embeds)
 
-@bot.slash_command(guild_ids=[618048657001938974])
+@bot.slash_command(guild_ids=[922599971461672961])
 async def fetch_deeds_by_county(ctx,county: Option(str,"County",Required=True)):
     data = db.fetchDeedsByCounty(county)
     for auction in range(len(data)):
@@ -35,10 +35,10 @@ async def fetch_deeds_by_county(ctx,county: Option(str,"County",Required=True)):
             embeds.add_field(name="Parcel URL:", value=deed['url'],inline=True)
             embeds.add_field(name="Property Address:", value=deed['property_address'],inline=True)
             embeds.add_field(name="Assessed Value:", value=deed['assessed_value'],inline=True)
-            embeds.add_field(name="Associated Website (County/Location)",value=data[auction]['location'])
+            #embeds.add_field(name="Associated Website (County/Location)",value=data[auction]['location'],inline=True)
             await ctx.send(embed=embeds)
-            
-@bot.slash_command(guild_ids=[618048657001938974])
+
+@bot.slash_command(guild_ids=[922599971461672961])
 async def auctions_30_day_range(ctx):
     auctions = db.fetchNearestAuctions(time.time(),time.time()+2592000) #30-day range
     auctions = sorted(auctions,key=lambda x:len(x['deeds']))
